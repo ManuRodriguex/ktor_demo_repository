e14aea34c1bda2f58b982e3b81f9ec2988449faf
Manu Rodriguez <manu@gmail.com>
2022-11-22 00:41:30 +0100

    it should work

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,16 @@
+import org.jetbrains.kotlin.com.intellij.openapi.vfs.StandardFileSystems.jar
+
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
 
 plugins {
-    application
+    //application
     kotlin("jvm") version "1.7.20"
+    //id("com.github.johnrengelman.shadow") version "7.1.2"
     id("io.ktor.plugin") version "2.1.3"
                 id("org.jetbrains.kotlin.plugin.serialization") version "1.7.21"
+
 }
 
 group = "com.example"
@@ -29,11 +33,19 @@ ktor {
     }
 }
 
+
+
+
+
 tasks {
     create("stage").dependsOn("installDist")
 }
 
+
+
 dependencies {
+    //implementation("com.github.jengelman.gradle.plugins:shadow:7.1.2")
+
     implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
